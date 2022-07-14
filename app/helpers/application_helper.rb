1c6025d8fbe65695e0e55315ae74b83086c23e03
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-07-14 21:15:54 +0200

    helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def cp(path)
         return true if current_page?(path)
     end
+
+    def truncate(string, max)
+        string.length > max ? "#{string[0...max]}..." : string
+    end
 end
