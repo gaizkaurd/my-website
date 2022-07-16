1c6545323bc31be0937097e7ff5b5ea096dd9759
Gaizka Urdangarin <gaizkaurdangarin@gmail.com>
2022-07-16 10:29:59 +0200

    temporaly remove trix

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,10 +1,8 @@
 import '@hotwired/turbo-rails';
-import '@rails/actiontext';
-import 'trix';
+/* import '@rails/actiontext';
+import 'trix'; */
 
 import.meta.globEager('../channels/**/*_channel.js');
 
 import '../utils/setupServiceWorker.js';
-import '../utils/setupHoneybadger.js';
-import '../utils/setupPlausible.js';
 import '../utils/setupStimulus.js';
